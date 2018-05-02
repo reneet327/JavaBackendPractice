e0c256aab11662fb3fd7f9f9d21f880318b98089
reneet327 <reneet327@gmail.com>
2018-05-01 21:22:31 -0700

    more projects

---
+++ buggy-app/src/main/java/com/example/demo/BasicController.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import java.time.LocalDateTime;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class BasicController {
+
+    @RequestMapping("/")
+    public @ResponseBody String greeting() {
+            String localDateTime = getLocalDateTime();
+        return localDateTime;
+    }
+
+    private String getLocalDateTime() {
+            String result = "";
+            LocalDateTime now = LocalDateTime.now();
+            result = now.toString();
+            System.out.println(result);
+            return result;
+    }
+}
